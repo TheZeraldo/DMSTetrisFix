1f5a6e200665854e37339ce8b83b0271e90d10e0
TheZeraldo <abdullahsayedbusiness@gmail.com>
2025-11-09 21:28:14 +0800

    Main Menu Added

--- TetrisGame/src/main/java/com/comp2042/Main.java
+++ TetrisGame/src/main/java/com/comp2042/Main.java
@@ -10,10 +10,33 @@ import java.net.URL;
 import java.util.ResourceBundle;
 
 public class Main extends Application {
+	
+	private Stage primaryStage;
 
     @Override
     public void start(Stage primaryStage) throws Exception {
+    	this.primaryStage = primaryStage;
+    	
+    	switch(GameStates.gameState) {
+    	
+    	case MENU:
+    		showMenu();
+    		break;
+    	case PLAYING:
+    		startGame();
+    		break;
+    	case GAME_OVER:
+    		//add GAME OVER screen
+    		break;
+    	}
+    }
 
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+    public void startGame() throws Exception {
         URL location = getClass().getClassLoader().getResource("gameLayout.fxml");
         ResourceBundle resources = null;
         FXMLLoader fxmlLoader = new FXMLLoader(location, resources);
@@ -21,14 +44,26 @@ public class Main extends Application {
         GuiController c = fxmlLoader.getController();
 
         primaryStage.setTitle("TetrisJFX");
-        Scene scene = new Scene(root, 300, 510);
+        Scene scene = new Scene(root, 400, 510);
         primaryStage.setScene(scene);
+        primaryStage.setResizable(false);
         primaryStage.show();
         new GameController(c);
     }
+    
+    public void showMenu() throws Exception {
+    	URL location = getClass().getClassLoader().getResource("menu.fxml");
+        ResourceBundle resources = null;
+        FXMLLoader fxmlLoader = new FXMLLoader(location, resources);
+        Parent root = fxmlLoader.load();
+        MenuController m = fxmlLoader.getController();
+        m.setPrimaryStage(primaryStage);
+        m.setMain(this);
 
-
-    public static void main(String[] args) {
-        launch(args);
+        primaryStage.setTitle("TetrisJFX");
+        Scene scene = new Scene(root, 400, 510);
+        primaryStage.setScene(scene);
+        primaryStage.setResizable(false);
+        primaryStage.show();
     }
 }
